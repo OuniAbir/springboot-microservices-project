ea1f1fe2b5bd404f87c6cd3b5ffc01ac6e9e286f
OuniAbir <ouniabir118@gmail.com>
2021-02-17 12:06:49 +0100

    order/product service point to microservices-config-file to fetch properties
    test Refreshing configuration - change test property in product-service.properties

---
+++ product-service/src/main/java/com/learning/productservice/controller/ReloadingPropertiesTest.java
@@ -0,0 +1,26 @@
+package com.learning.productservice.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+@RequestMapping("/api/test")
+@RefreshScope
+public class ReloadingPropertiesTest {
+
+	
+	@Value("${test.name}")
+	private String name;
+	
+	@GetMapping
+	public String test() {
+		return name ;
+		
+	}
+	
+	
+}
